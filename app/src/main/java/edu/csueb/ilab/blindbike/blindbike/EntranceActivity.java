828e9970b1b6b6cc3b22ba0634c983e1455a8299
William <woverell@horizon.csueastbay.edu>
2015-04-13 12:32:46 -0700

    Starting work on EntranceActivity.  Still to do: store coordinates from geocoder, stringify and send to NaviActivity.

--- app/src/main/java/edu/csueb/ilab/blindbike/blindbike/EntranceActivity.java
+++ app/src/main/java/edu/csueb/ilab/blindbike/blindbike/EntranceActivity.java
@@ -1,17 +1,49 @@
 package edu.csueb.ilab.blindbike.blindbike;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mapquest.android.maps.GeoPoint;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class EntranceActivity extends ActionBarActivity {
 
+    /**
+     * Coordinates of the destination to be sent to the NaviActivity
+     */
+    private double[] destination_coords;
+
+    // GUI Variables
+    private EditText edit_destination;
+
+    private Button button_options;
+
+    private Button button_go;
+
+    private Button button_help;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entrance);
+        setContentView(R.layout.activity_entrance_landscape);
+
+        setupGUI();
     }
 
 
@@ -35,5 +67,150 @@ public class EntranceActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Sets up the GUI elements
+     */
+    private void setupGUI(){
+        this.edit_destination = (EditText) findViewById(R.id.destinationEditText);
+
+        this.button_go = (Button) findViewById(R.id.goButton);
+        button_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get the entered destination
+                String str_destination = edit_destination.getText().toString();
+
+                if (str_destination.length() == 0) {
+                    // If no destination entered then tell user
+                    Toast.makeText(EntranceActivity.this,
+                            R.string.noDestinationEntered, Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    // Start asyc SearchDestinationTask to get coordinates from geocoder
+                    SearchDestinationTask destinationTask = new SearchDestinationTask();
+                    destinationTask.execute(str_destination);
+                    // Send coordinates to NaviActivity to start navigation
+
+                }
+            }
+        });
+    }
+
+    /**
+     * This function takes a latitude and longitude value and creates a string
+     * that mapquest knows how to read.
+     * @param latitude
+     * @param longitude
+     * @return String containing lat/long that mapquest can read
+     */
+    private String stringifyCoords(double latitude, double longitude){
+        return "{latLng:{lat:" + latitude + ",lng:" + longitude + "}}";
+    }
+
+    /**
+     * This is a class to search for the destination asynchronously.
+     *
+     * @author Marius Runde
+     */
+    private class SearchDestinationTask extends
+            AsyncTask<String, Void, GeoPoint> {
+
+        /**
+         * Progress dialog to inform the user about the searching process
+         */
+        private ProgressDialog progressDialog = new ProgressDialog(
+                EntranceActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            // Display progress dialog
+            progressDialog.setMessage("Searching for destination...");
+            progressDialog.show();
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    // Enable canceling the search
+                    SearchDestinationTask.this.cancel(true);
+                }
+            });
+        }
+
+        @Override
+        protected GeoPoint doInBackground(String... destination) {
+            String str_destination = destination[0];
+            List<Address> addresses;
+            try {
+                // Create a geocoder to locate the destination
+                Geocoder geocoder = new Geocoder(EntranceActivity.this,
+                        Locale.getDefault());
+                addresses = geocoder.getFromLocationName(str_destination,
+                        R.integer.MAX_RESULTS);
+            } catch (IOException e1) {
+                // Destination could not be located but try again once
+                // because sometimes it works at the second try
+                Log.d("MainActivity",
+                        "First try to locate destination failed. Starting second try...");
+                try {
+                    // Create a geocoder to locate the destination
+                    Geocoder geocoder = new Geocoder(EntranceActivity.this,
+                            Locale.getDefault());
+                    addresses = geocoder.getFromLocationName(str_destination,
+                            R.integer.MAX_RESULTS);
+                } catch (IOException e2) {
+                    // Seems like the destination could really not be
+                    // found, so send the user a message about the error
+                    Log.e("MainActivity",
+                            "IO Exception in searching for destination. This is the error message: "
+                                    + e2.getMessage());
+                    Toast.makeText(EntranceActivity.this,
+                            R.string.noDestinationFound, Toast.LENGTH_SHORT)
+                            .show();
+                    return null;
+                }
+            }
+
+            if (addresses.isEmpty()) {
+                // Destination could not be located
+                Toast.makeText(EntranceActivity.this, R.string.noDestinationFound,
+                        Toast.LENGTH_SHORT).show();
+                return null;
+            } else {
+                // Destination could be located
+                Log.d("MainActivity", "Located destination sucessfully.");
+                GeoPoint result = new GeoPoint(addresses.get(0).getLatitude(),
+                        addresses.get(0).getLongitude());
+                return result;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(GeoPoint result) {
+            // Dismiss progress dialog
+            progressDialog.dismiss();
+
+            // Check if the search was successful
+            if (result != null) {
+                // Store result in dest_coords
+                destination_coords = new double[] { result.getLatitude(),
+                        result.getLongitude() };
+
+                /*
+                // Create the destination overlay
+                addDestinationOverlay(result);
+
+                // If the route has been calculated before change the text
+                // of the button so the route has to be calculated again and
+                // clear the route from the RouteManager
+                if (btn_calculate.getText() == getResources().getString(
+                        R.string.start)) {
+                    btn_calculate.setText(R.string.calculate);
+                    rm.clearRoute();
+                }
+                */
+            }
+        }
     }
 }
