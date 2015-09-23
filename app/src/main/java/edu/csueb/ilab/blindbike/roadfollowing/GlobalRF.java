c44aea843b9a6d8070bdf1b6aae0de5518b154e1
William <woverell@horizon.csueastbay.edu>
2015-09-22 18:28:41 -0700

    Started implementation for Gaussian Mixture Models in GlobalRF.  Added data files for the 3 classes containing 1000 samples each of RGB values.

--- app/src/main/java/edu/csueb/ilab/blindbike/roadfollowing/GlobalRF.java
+++ app/src/main/java/edu/csueb/ilab/blindbike/roadfollowing/GlobalRF.java
@@ -1,6 +1,7 @@
 package edu.csueb.ilab.blindbike.roadfollowing;
 
 import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import org.opencv.ml.EM;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Vector;
 
 import edu.csueb.ilab.blindbike.blindbike.R;
 
@@ -97,14 +99,7 @@ public class GlobalRF {
         --------------------------
      */
 
-    EM road_em;
-    EM sky_em;
-    EM lanemarking_em;
-
-    /*
-        K-Nearest Neighbors class
-     */
-    CvKNearest knn;
+    Vector<GMM> gmms;
 
     /**
      * Constructor
@@ -114,13 +109,9 @@ public class GlobalRF {
         localFrame = new Mat();
         sample = new Mat();
 
-        // Termination Criteria: 0 - max iterations, COUNT is default max iters which is 100, EPS is the default EPS value
-        TermCriteria termCrit = new TermCriteria(0, TermCriteria.COUNT, TermCriteria.EPS);
+        gmms = new Vector();
 
-        // Initalize GMMs for each class
-        road_em = new EM(R.integer.ROAD_GMM_MODES, EM.COV_MAT_DIAGONAL, termCrit);
-        sky_em = new EM(R.integer.SKY_GMM_MODES, EM.COV_MAT_DIAGONAL, termCrit);
-        lanemarking_em = new EM(R.integer.LANEMARKING_GMM_MODES, EM.COV_MAT_DIAGONAL, termCrit);
+        gmms = Filter_Utility.readParametersForMixtureOfGaussiansForAllDataClasses(BB_Test_Train_Util.filepath_to_code_source + "gmmFileForAllDataClasses.dat");
 
         // Load training data
         // TODO: number of samples hard coded at 1000, change to however many samples are in file
@@ -138,43 +129,7 @@ public class GlobalRF {
             Log.i("WILL","From input reader:");
         }
 
-        // Train GMM for each class
-        road_em.train(road_data);
-        sky_em.train(sky_data);
-        lanemarking_em.train(lanemarking_data);
 
-        //region Unused KNN Test Code
-        /* K-NN TEST CODE
-        // Initialize K-NN class with training data
-        // TODO: ADD TRAINING DATA AS PARAMETER
-        int[][] intArray = new int[][]{{2,3,4},{5,6,7},{8,9,10},{9, 9, 7},{8,7,7}}; // points
-        Mat sampleData = new Mat(5,3, CvType.CV_32FC1); // 5 rows; 3 cols
-        for(int row=0;row<5;row++){
-            for(int col=0;col<3;col++)
-                sampleData.put(row, col, intArray[row][col]);
-        }
-
-        int[][]labelArray = new int[][]{{1}, {1}, {0}, {1}, {0}}; // labels
-        Mat labels = new Mat(5,1, CvType.CV_32FC1);  // 5 rows; 1 col
-        for(int row=0;row<5;row++){
-            for(int col=0;col<1;col++)
-                labels.put(row, col, labelArray[row][col]);
-        }
-        knn = new CvKNearest(sampleData, labels); // initialize and train knn
-
-        // Test knn
-        Mat results = new Mat();
-        Mat neighborResponses = new Mat();
-        Mat dists = new Mat();
-        Mat testSample = new Mat(1,3, CvType.CV_32FC1); // will have one entry: (2,3,4)
-        testSample.put(0, 0, 9);
-        testSample.put(0, 1, 9);
-        testSample.put(0, 2, 6);
-        knn.find_nearest(testSample, 3, results, neighborResponses, dists);
-        double resultClass = results.get(0,0)[0];
-        Log.i("NaviActivity", "class: " + resultClass + "\n");
-        */
-        //endregion
     }
 
     /**
@@ -191,7 +146,7 @@ public class GlobalRF {
         height = height / (R.integer.DOWNSAMPLE_TIMES * 2);
         width = width / (R.integer.DOWNSAMPLE_TIMES * 2);
         // Set region of interest rectangle, eliminate top REGION_OF_INTEREST_PERCENT percent
-        regionOfInterest = new Rect(0, (int) ((R.integer.REGION_OF_INTEREST_PERCENT / 100.0) * height), width, height);
+        this.regionOfInterest = new Rect(0, (int) ((R.integer.REGION_OF_INTEREST_PERCENT / 100.0) * height), width, height);
     }
 
     /**
@@ -218,9 +173,6 @@ public class GlobalRF {
             }
         }
         */
-        road_em.getDouble("covs");
-        double prob_road[] = new double[2];
-        prob_road = road_em.predict(sample);
     }
 
     /**
